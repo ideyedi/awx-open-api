3038bae5e5accad85b2314296773d0efadecf8f0
ideyedi <gdregas18@gmail.com>
2022-08-12 16:12:35 +0900

    Quick Starter
    Dev-batch jenkins 기준 minor function 추가

--- app/routers/jenkins.py
+++ app/routers/jenkins.py
@@ -5,10 +5,28 @@ from ..models.params import JenkinsJobParams
 from ..services.jenkins import JenkinsAgent
 from ..errors import raise_error
 
+import requests
+
 router = APIRouter(prefix="/jenkins", tags=["Jenkins"])
 agent = JenkinsAgent()
 
 
+@router.post('/jobs', response_class=PlainTextResponse)
+def account(profile, account_id: str, account_name: str, account_email: str):
+    domain = "http://jenkins.dev.wemakeprice.com"
+    url = domain + "/job/MAINTENANCE/job/wd_user/buildWithParameters"
+    account_data = {
+        'account_id': account_id,
+        'account_name': account_name,
+        'account_email': account_email
+    }
+
+    ret = requests.post(url, auth=("esji", "01eff24aa80f35ba589c3901c292064c"), data=account_data, timeout=5)
+    print(f'{ret}')
+    return "OK"
+
+
+'''
 @router.post("/jobs", response_class=PlainTextResponse)
 def create_job(params: JenkinsJobParams):
     job_url = agent.create(params)
@@ -25,10 +43,4 @@ def remove_job(self, project: str, name: str):
         raise_error(500, f"Failed to remove jenkins job: {project}/{name}")
 
     return {"removed": "OK"}
-
-
-@router.get('/jobs', response_class=PlainTextResponse)
-def account():
-    ret: int = 0
-    return ret
-
+'''
